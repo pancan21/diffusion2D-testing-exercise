1563463e414f8cf3584c7883052ab26a2d71cc1d
pancan <verifx@gmx.de>
2024-10-08 17:23:45 +0200

    rewrite tests using unittest class to refactor the setup calls

--- tests/unit/test_diffusion2d_functions.py
+++ tests/unit/test_diffusion2d_functions.py
@@ -6,90 +6,101 @@ from diffusion2d import SolveDiffusion2D
 import pytest
 import numpy as np
 
-def test_initialize_domain():
-    """
-    Check function SolveDiffusion2D.initialize_domain
-    """
-    solver = SolveDiffusion2D()
-    solver.initialize_domain(
-        w=5.,
-        h=4.,
-        dx=0.2,
-        dy=0.5
-    )
+from unittest import TestCase
+
+class TestDiffusion2DFunctions(TestCase):
     
-    # check assigned values
-    assert solver.w == 5.
-    assert solver.h == 4.
-    assert solver.dx == 0.2
-    assert solver.dy == 0.5
+    def setUp(self):
+        # create an instance of SolveDiffusion2D
+        self.solver = SolveDiffusion2D()
+        
+        # define fixture values
+        self.w = 5.
+        self.h = 4.
+        
+        self.dx = 0.2
+        self.dy = 0.5
+        
+        self.D = 5.
+        
+        self.T_cold = -300.5
+        self.T_hot = 800.
     
-    # check computed nx, ny
-    assert solver.nx == 25
-    assert solver.ny == 8
+    def test_initialize_domain(self):
+        """
+        Check function SolveDiffusion2D.initialize_domain
+        """
+        self.solver.initialize_domain(
+            w=self.w,
+            h=self.h,
+            dx=self.dx,
+            dy=self.dy
+        )
+        
+        # check assigned values
+        assert self.solver.w == self.w
+        assert self.solver.h == self.h
+        assert self.solver.dx == self.dx
+        assert self.solver.dy == self.dy
+        
+        # check nx and ny
+        assert self.solver.nx == 25
+        assert self.solver.ny == 8
 
 
-def test_initialize_physical_parameters():
-    """
-    Checks function SolveDiffusion2D.initialize_physical_parameters
-    """
-    solver = SolveDiffusion2D()
-    solver.w = 5.
-    solver.h = 5.
-    solver.dx = 0.2
-    solver.dy = 0.5
-    
-    solver.nx = 25
-    solver.ny = 10
-    
-    solver.initialize_physical_parameters(
-        d=5.,
-        T_cold=-300.5,
-        T_hot=800.
-    )
-    
-    # check assigned values
-    assert solver.D == 5.
-    assert solver.T_cold == -300.5
-    assert solver.T_hot == 800.
-    
-    # check dt
-    print(solver.dt)
-    expected_dt = 0.003448275
-    pytest.approx(solver.dt, expected_dt, 1e-6)
+    def test_initialize_physical_parameters(self):
+        """
+        Checks function SolveDiffusion2D.initialize_physical_parameters
+        """
+        self.solver.w = self.w
+        self.solver.h = self.h
+        self.solver.dx = self.dx
+        self.solver.dy = self.dy
+        
+        self.solver.initialize_physical_parameters(
+            d=self.D,
+            T_cold=self.T_cold,
+            T_hot=self.T_hot
+        )
+        
+        # check assigned values
+        assert self.solver.D == self.D
+        assert self.solver.T_cold == self.T_cold
+        assert self.solver.T_hot == self.T_hot
+        
+        # check dt      
+        expected_dt = 0.003448275
+        pytest.approx(self.solver.dt, expected_dt, 1e-6)
 
 
-def test_set_initial_condition():
-    """
-    Checks function SolveDiffusion2D.get_initial_function
-    """
-    solver = SolveDiffusion2D()
-    
-    solver.w = 5.
-    solver.h = 5.
-    solver.dx = 0.2
-    solver.dy = 0.5
-    
-    solver.nx = 25
-    solver.ny = 10
-    
-    solver.D = 5.
-    solver.T_cold = -300.5
-    solver.T_hot = 800.
-    
-    u = solver.set_initial_condition()
-    
-    # create expected shape
-    expected_u = np.ones((25, 10)) * -300.5
-    r = min(solver.h, solver.w) / 4.0
-    cx = solver.w / 2.0
-    cy = solver.h / 2.0
-    r2 = r ** 2
-    for i in range(solver.nx):
-        for j in range(solver.ny):
-            p2 = (i * solver.dx - cx) ** 2 + (j * solver.dy - cy) ** 2
-            if p2 < r2:
-                expected_u[i, j] = solver.T_hot
+    def test_set_initial_condition(self):
+        """
+        Checks function SolveDiffusion2D.get_initial_function
+        """
+        self.solver.w = self.w
+        self.solver.h = self.h
+        self.solver.dx = self.dx
+        self.solver.dy = self.dy
+        
+        self.solver.T_cold = self.T_cold
+        self.solver.T_hot = self.T_hot
+        
+        self.solver.nx = 25
+        self.solver.ny = 10
+        
+        u = self.solver.set_initial_condition()
+        
+        # create expected shape
+        expected_u = np.ones((25, 10)) * -300.5
+        r = min(self.solver.h, self.solver.w) / 4.0
+        cx = self.solver.w / 2.0
+        cy = self.solver.h / 2.0
+        r2 = r ** 2
+        for i in range(self.solver.nx):
+            for j in range(self.solver.ny):
+                p2 = (i * self.solver.dx - cx) ** 2 + (j * self.solver.dy - cy) ** 2
+                if p2 < r2:
+                    expected_u[i, j] = self.solver.T_hot
                     
     # check values
     assert np.allclose(u, expected_u)
