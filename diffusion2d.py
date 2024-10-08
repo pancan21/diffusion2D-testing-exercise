dfccea2e474f9ae89e71bf13c2bcbab7ecc97bf1
pancan <verifx@gmx.de>
2024-10-08 16:18:16 +0200

    add assertions to check if inputs are float types

--- diffusion2d.py
+++ diffusion2d.py
@@ -38,6 +38,7 @@ class SolveDiffusion2D:
         self.dt = None
 
     def initialize_domain(self, w=10., h=10., dx=0.1, dy=0.1):
+        assert all(isinstance(i, float) for i in [w, h, dx, dy]), "All arguments must be floats"
         self.w = w
         self.h = h
         self.dx = dx
@@ -45,7 +46,8 @@ class SolveDiffusion2D:
         self.nx = int(w / dx)
         self.ny = int(h / dy)
 
-    def initialize_physical_parameters(self, d=4., T_cold=300, T_hot=700):
+    def initialize_physical_parameters(self, d=4., T_cold=300., T_hot=700.):
+        assert all(isinstance(i, float) for i in [d, T_cold, T_hot]), "All arguments must be floats"
         self.D = d
         self.T_cold = T_cold
         self.T_hot = T_hot
